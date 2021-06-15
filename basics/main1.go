faf4a95ea5d40737dbff643eb4cd6ecd4244e405
Samantha-Grace <sgrace732@gmail.com>
2021-06-15 15:10:18 -0600

    first commit

---
+++ basics/main1.go
@@ -0,0 +1,16 @@
+//type conversion
+package main
+
+import "fmt"
+
+func main() {
+	speed := 100
+	force := 2.5
+
+	//speed = speed * intforce
+	//speed = speed * int(force)
+
+	speed = int(float64(speed) * force)
+
+	fmt.Println(speed)
+}
